b54cb882502300b1639ee6dba1d57aa79c1c8440
hyerimmy <heyrim2010@naver.com>
2023-11-10 19:13:59 +0900

    feat: 예약 설정 기능 완료 - inputReservationDate - inputMenus

--- src/main/java/christmas/controller/EventController.java
+++ src/main/java/christmas/controller/EventController.java
@@ -1,4 +1,19 @@
 package christmas.controller;
 
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
 public class EventController {
+    public EventController() {
+    }
+
+    public void run() {
+        OutputView.printWelcomeMessage();
+        takeReservation();
+    }
+
+    private void takeReservation() {
+        InputView.inputReservationDate();
+        InputView.inputMenus();
+    }
 }
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,4 +1,11 @@
 package christmas.view;
 
+import christmas.constant.message.OutputMessage;
+import christmas.utils.Utils;
+
 public class OutputView {
+
+    public static void printWelcomeMessage() {
+        System.out.println(OutputMessage.WELCOME_MESSAGE);
+    }
 }
