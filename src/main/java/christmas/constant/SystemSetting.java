b4b6a59cb78880e945059d554f0b66a247554ae5
hyerimmy <heyrim2010@naver.com>
2023-11-11 23:51:11 +0900

    chore: 이벤트 진행 달(MONTH) 상수 정의

--- src/main/java/christmas/constant/SystemSetting.java
+++ src/main/java/christmas/constant/SystemSetting.java
@@ -4,5 +4,6 @@ import java.text.DecimalFormat;
 
 public class SystemSetting {
     public static final int YEAR = 2023;
+    public static final int MONTH = 12;
     public static final DecimalFormat MONEY_FORMAT = new DecimalFormat("###,##0");
 }
