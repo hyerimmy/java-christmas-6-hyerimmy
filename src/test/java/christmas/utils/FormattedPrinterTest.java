ea76272baa98306774f1b3f6b73e2d06196cee5a
hyerimmy <heyrim2010@naver.com>
2023-11-15 03:38:28 +0900

    test: FormattedPrinterTest.java 테스트 코드 작성 - 예외 메시지 출력 포맷팅 검증 - 헤더 메시지 출력 검증

---
+++ src/test/java/christmas/utils/FormattedPrinterTest.java
@@ -0,0 +1,53 @@
+package christmas.utils;
+
+import camp.nextstep.edu.missionutils.Console;
+import org.junit.jupiter.api.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class FormattedPrinterTest {
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @BeforeEach
+    void init(){
+        System.setOut(new PrintStream(out));
+    }
+
+    @Nested
+    @DisplayName("형식화된 문자열 프린트 테스트")
+    class ReadNumberTest {
+        @Test
+        @DisplayName("입력된 예외 메시지가 다시 입력해주세요 문구와 함께 출력된다.")
+        void exceptionMessage() {
+            // given
+            final String exceptionMessage = "숫자 형식이 아닙니다.";
+
+            // when
+            FormattedPrinter.printlnExceptionWithReEnterMessage(exceptionMessage);
+
+            // then
+            final String answer = "숫자 형식이 아닙니다. 다시 입력해 주세요.";
+            assertThat(out.toString()).isEqualToIgnoringNewLines(answer);
+        }
+
+        @Test
+        @DisplayName("입력된 메시지를 헤더 형식으로 출력한다.")
+        void headerMessage() {
+            // given
+            final String headerMessage = "제목";
+
+            // when
+            FormattedPrinter.printlnHeaderMessage(headerMessage);
+
+            // then
+            final String answer = "<제목>";
+            assertThat(out.toString()).isEqualToIgnoringNewLines(answer);
+        }
+    }
+}
