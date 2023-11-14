204ff829cf1fe3e71262bd93756cc1e110639144
hyerimmy <heyrim2010@naver.com>
2023-11-15 01:13:24 +0900

    test: 방문일 입력 테스트 추가 - 범위를 벗어난 경우 예외처리 검증 추가

--- src/test/java/christmas/ApplicationTest.java
+++ src/test/java/christmas/ApplicationTest.java
@@ -4,6 +4,8 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import christmas.controller.EventController;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
@@ -33,12 +35,28 @@ class ApplicationTest extends NsTest {
         });
     }
 
-    @Test
-    void 날짜_예외_테스트() {
-        assertSimpleTest(() -> {
-            runException("a");
-            assertThat(output()).contains("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
-        });
+    @Nested
+    class 방문일_입력_테스트{
+        @Test
+        void 문자열을_입력할_경우_예외_처리한다() {
+            assertSimpleTest(() -> {
+                runException("a");
+                assertThat(output()).contains("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+            });
+        }
+
+        @Test
+        void 범위를_벗어난_숫자를_입력할_경우_예외_처리한다() {
+            assertSimpleTest(() -> {
+                runException("0");
+                assertThat(output()).contains("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+            });
+
+            assertSimpleTest(() -> {
+                runException("32");
+                assertThat(output()).contains("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+            });
+        }
     }
 
     @Test
---
+++ src/test/java/christmas/controller/EventControllerTest.java
@@ -0,0 +1,12 @@
+package christmas.controller;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class EventControllerTest {
+
+
+
+}
+
+
