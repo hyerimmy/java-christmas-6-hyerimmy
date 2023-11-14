7ffcf1c71474ca45c130e3dfe9eb76e8d34b6be3
hyerimmy <heyrim2010@naver.com>
2023-11-14 17:55:41 +0900

    refactor: 특별할인 대상 날짜 상수리스트로 정의해 사용

--- src/main/java/christmas/model/benefit/SpecialDiscount.java
+++ src/main/java/christmas/model/benefit/SpecialDiscount.java
@@ -3,28 +3,34 @@ package christmas.model.benefit;
 import christmas.model.Plan;
 
 import java.time.LocalDate;
+import java.util.List;
+
+import static christmas.constant.SystemSetting.MONTH;
+import static christmas.constant.SystemSetting.YEAR;
 
 public class SpecialDiscount extends Benefit {
     private static final SpecialDiscount instance = new SpecialDiscount();
     private final static int discountAmount = 1_000;
+    private final static List<LocalDate> specialDates = List.of(
+            LocalDate.of(YEAR, MONTH, 3),
+            LocalDate.of(YEAR, MONTH, 10),
+            LocalDate.of(YEAR, MONTH, 17),
+            LocalDate.of(YEAR, MONTH, 25),
+            LocalDate.of(YEAR, MONTH, 31)
+    );
 
     public SpecialDiscount() {
         super("특별 할인");
     }
 
-    protected static SpecialDiscount getInstance(){
+    protected static SpecialDiscount getInstance() {
         return instance;
     }
 
     @Override
     public boolean applyToDiscount(Plan plan) {
-        LocalDate dayOfVisit = plan.getDateOfVisit();
-        return (dayOfVisit.getDayOfMonth() == 3
-                || dayOfVisit.getDayOfMonth() == 10
-                || dayOfVisit.getDayOfMonth() == 17
-                || dayOfVisit.getDayOfMonth() == 25
-                || dayOfVisit.getDayOfMonth() == 31
-        );
+        LocalDate dateOfVisit = plan.getDateOfVisit();
+        return specialDates.contains(dateOfVisit);
     }
 
     @Override
